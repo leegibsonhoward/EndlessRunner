d0ff9f2da8f8d32a89e5f7c252d0226de0be1516
leehowardx <git@leehoward.dev>
2022-10-09 22:25:03 -0700

    add sdl2 and configure makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
-INCLUDES= -I ./include
-FLAGS= -g
+INCLUDES= -I./include -I./external/SDL2/include
+EXTERN_LIB_PATH= -L ./external/SDL2/lib
+LIBRARIES= ${EXTERN_LIB_PATH} -lmingw32 -lSDL2main -lSDL2
+SRC_FILE= ./src/main.c
+OUTPUT_FILE= -o ./bin/main
+FLAGS= -g -w
+
 all:
-	gcc ${FLAGS} ${INCLUDES} ./src/main.c -o ./bin/main
+	gcc ${FLAGS} ${INCLUDES} ${SRC_FILE} ${LIBRARIES} ${OUTPUT_FILE}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include "SDL2/SDL.h"
 
-int main(int argc, char ** argv)
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 320
+int main(int argc, char **argv)
 {
-    printf("Hello World!");
-    return 0;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow(
+        "EndlessRunner",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+    while (1)
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        SDL_Rect r;
+        // draw rect in middle of screen
+        r.x = WINDOW_WIDTH / 2 - 20;
+        r.y = WINDOW_HEIGHT / 2 - 20;
+        r.w = 40;
+        r.h = 40;
+        SDL_RenderDrawRect(renderer, &r);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
     
+    return 0;
+
 }
